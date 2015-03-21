ccdc1723651341971798479d69c94c80992ae380
Shi Chen <shi.chen@me.com>
2015-03-21 17:28:38 +0100

    Server update

--- script/play_update.sh
+++ script/play_update.sh
@@ -4,6 +4,8 @@ cd /home/pi/git/rpi-script
 git pull
 
 cp /etc/wpa_supplicant/wpa_supplicant.conf /home/pi/git/rpi-script/config-data
+git add /home/pi/git/rpi-script
+git commit -m "client update"
 
 # My own playlist
 #/home/pi/git/rpi-script/script/play_list.sh PLb89ENKNeEt4VcZjK2hxHxioq6st0IdkK
