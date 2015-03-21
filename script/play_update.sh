7e8fea0a42825ee097c03aa9f831eecb312f31a2
Shi Chen <shi.chen@me.com>
2015-03-21 16:53:35 +0100

    Change playlist to happy camp

--- script/play_update.sh
+++ script/play_update.sh
@@ -2,5 +2,9 @@
 
 cd /home/pi/git/rpi-script
 git pull
-/home/pi/git/rpi-script/script/play_list.sh PLb89ENKNeEt4VcZjK2hxHxioq6st0IdkK
+# my own playlist
+#/home/pi/git/rpi-script/script/play_list.sh PLb89ENKNeEt4VcZjK2hxHxioq6st0IdkK
+
+# Happy camp
+/home/pi/git/rpi-script/script/play_list.sh PLUM8x224JrX8b9GEQFHUzkUr1ZVbdtpkW
 
